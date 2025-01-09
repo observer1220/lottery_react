e0bfa29a6784d22db667d324558072d0b5991ce0
observer1220 <observer1220@gmail.com>
2025-01-09 14:38:37 +0800

    2025-01-09

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Step 1: 前端打包
-echo "Building the frontend..."
-npm run build || error_exit "Error: Frontend build failed"
-
-# Step 2: 將原始碼更新至 Github main branch
+# Step 1: 將原始碼更新至 Github main branch
 echo "Updating source code to GitHub..."
 git add . || error_exit "Error: Git add failed"
 current_date=$(date +"%Y-%m-%d")
