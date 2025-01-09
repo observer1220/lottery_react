89bbe57765a94c0ed61fa5defc30e8b24cb376eb
observer1220 <observer1220@gmail.com>
2025-01-09 14:27:57 +0800

    2025-01-09

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,3 @@ git add . || error_exit "Error: Git add failed"
 current_date=$(date +"%Y-%m-%d")
 git commit -m "$current_date" || error_exit "Error: Git commit failed"
 git push origin main || error_exit "Error: Git push failed"
-
-# Step 3: 將打包後的檔案更新至 Github gh-pages branch
-echo "Updating build files to GitHub gh-pages..."
-git subtree push --prefix build origin gh-pages || error_exit "Error: Git subtree push failed"
-echo "Deployment completed successfully!"
